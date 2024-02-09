558dc309f892e523531697fd97f30396d151c6a1
regelcabrales <regelcabrales@gmail.com>
2024-02-09 09:43:28 +0800

    Added content to pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,16 @@
+import './styles/Home.css';
+
 function Home() {
     return(
-        <div>
-            This is the homepage.
+        <div className="home-div">
+            <section className="main">
+                <h1>Where Home Find Their Stories</h1>
+                <p className="homeP">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, error
+                maxime debitis veniam dolor, assumenda hic vitae id nihil perferendis
+                facilis! Sed autem quas ipsam, eos optio natus dolorem corporis.
+                </p>
+            </section>
         </div>
     )
 }
